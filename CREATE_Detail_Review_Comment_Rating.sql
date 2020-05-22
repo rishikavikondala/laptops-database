eb4cb049a5da1c625cab5f0562cf7352d8363f62
rkim205-1839070 <63337084+rkim205-1839070@users.noreply.github.com>
2020-05-21 22:38:08 -0700

    Add files via upload

--- CREATE_Detail_Review_Comment_Rating.sql
+++ CREATE_Detail_Review_Comment_Rating.sql
@@ -3,7 +3,15 @@ USE INFO330_Proj_4
 CREATE TABLE tblDETAIL (
 	DetailID INT IDENTITY (1, 1) PRIMARY KEY,
 	DetailName varchar(20),
-	DetailDescription varchar(500)
+	DetailDescription varchar(500),
+	LaptopID INT,
+	Price INT,
+	Weight INT,
+	Color varchar(20),
+	BatteryLife FLOAT,
+	ReleaseYear INT
+	CONSTRAINT FK_LaptopID FOREIGN KEY (LaptopID)
+    REFERENCES tblLaptop(LaptopID)
 )
 GO
 
@@ -19,16 +27,15 @@ CREATE TABLE tblREVIEW (
   CONSTRAINT FK_LaptopOrderID FOREIGN KEY(LaptopOrderID)
   REFERENCES tblLAPTOP_ORDER(LaptopOrderID)
 )
-GO
+GO 
 
 CREATE TABLE tblCOMMENT (
 	CommentID INT IDENTITY (1, 1) PRIMARY KEY,
 	CommentDescription varchar(500)
 )
-GO
-
+GO 
 CREATE TABLE tblRATING (
 	RatingID INT IDENTITY (1, 1) PRIMARY KEY,
 	RatingValue INT
 )
-GO
+
