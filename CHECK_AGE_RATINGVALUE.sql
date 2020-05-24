428e0abfa4670673088e60ffe5a1c5b4965ec4e3
rkim205-1839070 <63337084+rkim205-1839070@users.noreply.github.com>
2020-05-23 17:14:25 -0700

    Add files via upload

---
+++ CHECK_AGE_RATINGVALUE.sql
@@ -0,0 +1,33 @@
+USE INFO330_Proj_4
+
+--A customer cannot be under 18 years of age.
+CREATE FUNCTION fn_NoYounger18Order() 
+RETURNS INT
+AS
+BEGIN
+	DECLARE @RET INT = 0
+	IF EXISTS (SELECT CustomerID FROM tblCUSTOMER 
+				WHERE DateOfBirth > DateAdd(Year, -18, GetDate())
+	SET @RET = 1
+RETURN @RET
+END
+GO
+ALTER TABLE tblORDER
+ADD CONSTRAINT CK_MustBe18
+CHECK (dbo.fn_NoYounger18Order() = 0)
+
+--Rating values must be between 1 and 5 inclusive
+CREATE FUNCTION fn_RatingOnly1to5()
+RETURNS INT
+AS 
+BEGIN
+	DECLARE @RET INT = 0
+	IF EXISTS (SELECT RatingValue FROM tblRATING	
+				WHERE RatingValue NOT BETWEEN 1 AND 5)
+	SET @RET = 1
+RETURN @RET
+END
+GO
+ALTER TABLE tblRATING
+ADD CONSTRAINT CK_Rating1to5
+CHECK (dbo.fn_RatingOnly1to5() = 0)
