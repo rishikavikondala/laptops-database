f695a4fc16f28d6f6e6fae7c01ad87f95a67556f
Rishi <rishi.kavikondala@gmail.com>
2020-05-31 00:44:18 -0700

    Updated WindowsWeight check constraint

--- Check Constraints/CHECK_WindowsWeight_NvidiaGraphics.sql
+++ Check Constraints/CHECK_WindowsWeight_NvidiaGraphics.sql
@@ -1,67 +1,72 @@
 USE INFO330_Proj_4
 
 /*
-No laptop released in or after 2015 with:
+No product of type laptop  released in or after 2015 with:
 - the feature ‘Windows’ (for an operating system)
 - Windows versions '10 Home' or '10 Pro'
 can have a weight of less than 1 pound.
 */
 
 GO
-CREATE FUNCTION fn_WindowsWeight() 
+ALTER FUNCTION fn_WindowsWeight() 
 RETURNS INT
 AS
 BEGIN
 	DECLARE @RET INT = 0
 	IF EXISTS (
-		SELECT L.LaptopID
-        FROM tblLAPTOP L
-            JOIN tblRELEASE_YEAR RY ON L.ReleaseYearID = RY.ReleaseYearID
-            JOIN tblLAPTOP_FEATURE LF ON L.LaptopID = LF.LaptopID
-            JOIN tblFEATURE F ON LF.FeatureID = F.FeatureID
+		SELECT DISTINCT P.ProductID
+        FROM tblPRODUCT P
+            JOIN tblPRODUCT_TYPE PT ON P.ProductTypeID = PT.ProductTypeID
+            JOIN tblPRODUCT_FEATURE PF ON P.ProductID = PF.ProductID
+            JOIN tblFEATURE F ON PF.FeatureID = F.FeatureID
             JOIN tblOS O ON F.FeatureID = O.FeatureID
             JOIN tblVERSION V ON O.VersionID = V.VersionID
-            JOIN tblWEIGHT W ON L.WeightID = W.WeightID
-        WHERE RY.ReleaseYear >= 2015
-		AND F.FeatureName = 'Windows'
+        WHERE P.ReleaseYear >= '2015'
+        AND F.FeatureName = 'Windows'
         AND V.VersionName = '10 Home'
         OR V.VersionName = '10 Pro'
-        AND W.Weight < 1.0
+        AND P.Weight < 1.0
 	)
 	SET @RET = 1
 RETURN @RET
 END
 GO
-ALTER TABLE tblLAPTOP_FEATURE
+ALTER TABLE tblPRODUCT
 ADD CONSTRAINT CK_WindowsWeight
 CHECK (dbo.fn_WindowsWeight() = 0)
 
--- No laptop released after the end of 2015 with the operating system feature MacOS and OS version Catalina can have a graphics card made by brand 'NVIDIA'.
+SELECT * FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+WHERE TABLE_NAME='tblPRODUCT'
 
-GO
-CREATE FUNCTION fn_NvidiaGraphics()
-RETURNS INT
-AS
-BEGIN
-    DECLARE @RET INT = 0
-    IF EXISTS (
-        SELECT L.LaptopID
-        FROM tblLAPTOP L
-            JOIN tblRELEASE_YEAR RY ON L.ReleaseYearID = RY.ReleaseYearID
-            JOIN tblLAPTOP_FEATURE LF ON L.LaptopID = LF.LaptopID
-            JOIN tblFEATURE F ON LF.FeatureID = F.FeatureID
-            JOIN tblBRAND B ON F.BrandID = B.BrandID
-            JOIN tblOS O ON F.FeatureID = O.FeatureID
-            JOIN tblVERSION V ON O.VersionID = V.VersionID
-        WHERE RY.ReleaseYear > '2015'
-        AND F.FeatureName = 'MacOS'
-        AND V.VersionName = 'Catalina'
-        AND B.BrandName = 'NVIDIA'
-    )
-    SET @RET = 1
-RETURN @RET
-END
-GO
-ALTER TABLE tblLAPTOP_FEATURE
-ADD CONSTRAINT CK_NvidiaGraphics
-CHECK (dbo.fn_NvidiaGraphics() = 0)
+ALTER TABLE tblPRODUCT
+DROP CONSTRAINT CK_WindowsWeight
+
+-- -- No laptop released after the end of 2015 with the operating system feature MacOS and OS version Catalina can have a graphics card made by brand 'NVIDIA'.
+
+-- GO
+-- CREATE FUNCTION fn_NvidiaGraphics()
+-- RETURNS INT
+-- AS
+-- BEGIN
+--     DECLARE @RET INT = 0
+--     IF EXISTS (
+--         SELECT L.LaptopID
+--         FROM tblLAPTOP L
+--             JOIN tblRELEASE_YEAR RY ON L.ReleaseYearID = RY.ReleaseYearID
+--             JOIN tblLAPTOP_FEATURE LF ON L.LaptopID = LF.LaptopID
+--             JOIN tblFEATURE F ON LF.FeatureID = F.FeatureID
+--             JOIN tblBRAND B ON F.BrandID = B.BrandID
+--             JOIN tblOS O ON F.FeatureID = O.FeatureID
+--             JOIN tblVERSION V ON O.VersionID = V.VersionID
+--         WHERE RY.ReleaseYear > '2015'
+--         AND F.FeatureName = 'MacOS'
+--         AND V.VersionName = 'Catalina'
+--         AND B.BrandName = 'NVIDIA'
+--     )
+--     SET @RET = 1
+-- RETURN @RET
+-- END
+-- GO
+-- ALTER TABLE tblLAPTOP_FEATURE
+-- ADD CONSTRAINT CK_NvidiaGraphics
+-- CHECK (dbo.fn_NvidiaGraphics() = 0)
--- README.md
+++ README.md
@@ -1,21 +1,23 @@
 # Laptops Database
 
-Collaborators:
+**Collaborators:**
 - Rishi Kavikondala
 - Tanner Kooistra
 - Brian Ying
 - Ryan Kim
 
-Lookup tables:
+**Lookup tables:**
 - tblBRAND_TYPE
-- Laptop details (tblCOLOR, tblPRICE, tblWEIGHT, tblBATTERY_LIFE)
-- tblCOMMENT
+- tblPRODUCT_TYPE
+- tblCOLOR
 - tblRATING
-- Feature subtype characteristics (tblVERSION, tblDISPLAY_SIZE, tblRESOLUTION, tblCLOCK_SPEED, tblNUM_CORES, tblRELEASE_YEAR, tblSECURITY_TYPE, tblPROGRESSIVE_SCAN, tblNUM_SPEAKERS, tblSTORAGE_SIZE)
+- tblVERSION
+- tblSECURITY_TYPE
 
-Stored procedures:
+**Stored procedures:**
 - tblCUSTOMER (Ryan)
 - tblREVIEW (Ryan)
+- tblCOMMENT (Rishi)
 - tblFEATURE (Rishi)
 - tblBRAND (Rishi)
 - Feature subtypes (Rishi)
@@ -27,12 +29,12 @@ Stored procedures:
     - tblWEBCAM
     - tblAUDIO
     - tblSTORAGE
-- tblLAPTOP (Tanner)
-- tblLAPTOP_FEATURE (Tanner)
-- tblLAPTOP_ORDER (Brian)
+- tblPRODUCT (Tanner)
+- tblPRODUCT_FEATURE (Tanner)
+- tblPRODUCT_ORDER (Brian)
 - tblORDER (Brian)
 
-Check constraints:
+**Check constraints:**
 - 
 - 
 - 
@@ -41,7 +43,7 @@ Check constraints:
 - 
 - 
 
-Computed columns:
+**Computed columns:**
 - 
 - 
 -
@@ -51,7 +53,7 @@ Computed columns:
 -
 -
 
-Views:
+**Views:**
 -
 -
 -
