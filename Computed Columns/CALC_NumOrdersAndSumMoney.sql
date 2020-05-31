0bd193681698f1000f638e0e9bc0f41ccc4fd75f
TannerKooi <46580993+TannerKooi@users.noreply.github.com>
2020-05-31 14:55:11 -0700

    Update CALC_NumOrdersAndSumMoney.sql

---
+++ Computed Columns/CALC_NumOrdersAndSumMoney.sql
@@ -0,0 +1,46 @@
+USE INFO330_Proj_4
+
+-- Computed Column for total number of orders a customer has placed
+
+GO
+CREATE FUNCTION CalcNumOrdersPerCustomer(@PK INT)
+RETURNS
+INT
+AS
+BEGIN
+DECLARE @RET INT = (
+    SELECT COUNT(DISTINCT O.OrderID)
+    FROM tblORDER O
+        JOIN tblCUSTOMER C ON O.CustomerID = C.CustomerID
+    WHERE C.CustomerID = @PK
+)
+RETURN @RET
+END
+GO
+ALTER TABLE tblCUSTOMER
+ADD NumOrdersPerCustomer AS (dbo.CalcNumOrdersPerCustomer(CustomerID))
+
+
+-- Computed Column for how much money a customer has spent on products of type laptop
+
+GO
+CREATE FUNCTION CalcMoneySpentOnLaptopsPerCustomer(@PK INT)
+RETURNS
+INT
+AS
+BEGIN
+DECLARE @RET INT = (
+    SELECT SUM(O.OrderTotal)
+    FROM tblORDER O
+        JOIN tblPRODUCT_ORDER PO ON  O.OrderID = PO.OrderID
+        JOIN tblPRODUCT P ON PO.ProductID = P.ProductID
+        JOIN tblPRODUCT_TYPE PT PN PT.ProductTypeID = P.ProductTypeID
+        JOIN tblCUSTOMER C ON O.CustomerID = C.CustomerID
+    WHERE P.ProductTypeName = 'Laptop'
+    AND C.CustomerID = @PK
+)
+RETURN @RET
+END
+GO
+ALTER TABLE tblCUSTOMER
+ADD SumMoneySpentOnLaptopsPerCustomer AS (dbo.CalcMoneySpentOnLaptopsPerCustomer(CustomerID))
